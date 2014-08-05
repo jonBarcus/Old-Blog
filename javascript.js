e89daf65d3e4344be0c1a372549c0a28adbf74e1
jonBarcus <barcus.j@gmail.com>
2014-08-05 15:03:28 -0400

    added AngelList to contacts.  Also updated JS to hide contact paragraphs on load.

--- javascript.js
+++ javascript.js
@@ -2,29 +2,34 @@ var myPortfolio = {
 
 	onReady: function(){
 		myPortfolio.hideResume();
+		myPortfolio.hideContactLinks();
 		$("#resume").click(myPortfolio.showResume);
 		$("#projects").click(myPortfolio.showProjects);
 		$("#contact").click(myPortfolio.showContact);
 	},
-	
+
 	hideResume: function(){
 		$("#resume-pdf").hide();
 	},
-		
+
+	hideContactLinks: function(){
+		$(".sub-menu").hide();
+	},
+
 	showResume: function(){
 		$("#resume").toggleClass("menu-expanded");
 		$("#resume-pdf").slideToggle();
 	},
-	
+
 	showProjects: function(){
 		$("#projects").toggleClass("menu-expanded");
 	},
-	
+
 	showContact: function(){
 		$("#contact").toggleClass("menu-expanded");
 	}
-	
-	
+
+
 }
 
 $(document).ready(myPortfolio.onReady);
